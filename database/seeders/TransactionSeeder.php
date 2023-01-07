cd23ae3e042f362248be06b6c4a49ef5d401507b
sebastian-huamani <huamanitassar@mail.com>
2023-01-07 11:13:06 -0500

    data faker

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -3,10 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Card;
-use App\Models\Item;
-use App\Models\Template;
-use App\Models\Transaction;
-use App\Models\User;
 use Carbon\Carbon;
 use DateTimeZone;
 use Illuminate\Database\Seeder;
@@ -26,7 +22,7 @@ class TransactionSeeder extends Seeder
             for ($i=0; $i < 80; $i++) { 
 
                 $template = rand(1, 150);
-                $amount = rand(-1000, 1000);
+                $amount = rand(-100000, 100000 ) / 100;
     
                 $card->items()->create(['title'=> 'voluptas occaecati et', 'body' => [["name","canela"], ["test","135.99"], ["count","as"], ["col3", "cat"]],'amount'=> $amount ,'template_id' => $template, 'created_at' => $toDay->addDays(1)]);
             }
