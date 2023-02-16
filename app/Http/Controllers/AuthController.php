76eda05fd3682ca4f4679d338cafd7aeaa32bb99
sebastian-huamani <huamanitassar@mail.com>
2023-02-16 11:22:30 -0500

    login verify

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,23 +15,22 @@ class AuthController extends Controller
 {
     public function register(AuthRequest $request)
     {
-        try{
-            $user = User::create([   
+        try {
+            $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'lastname' => $request->lastname,
                 'password' => Hash::make($request->password)
             ]);
-    
+
             $token = $user->createToken('auth_token')->plainTextToken;
-    
+
             return response()->json([
                 'res' => true,
                 'access_token' => $token,
                 'token_type' => 'Bearer'
             ], 200);
-
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
                 'res' => false,
                 'e' => $e->getMessage()
@@ -41,14 +40,22 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        if( !Auth::attempt($request->only('email', 'password')) ){
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
                 'res' => false,
-                'msg' => 'Credenciales Invalidas',
+                'msg' => 'Correo o Contraseña Invalidas',
             ], 401);
         }
 
         $user = User::where('email', $request->email)->firstOrFail();
+
+        if ($user->email_verified_at == null) {
+            return response()->json([
+                'res' => false,
+                'msg' => 'Esta cuenta aun no esta verificada'
+            ]);
+        }
+
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
@@ -58,7 +65,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function infoUser(Request $request )
+    public function infoUser(Request $request)
     {
         return response()->json(
             $request->user()
@@ -69,12 +76,12 @@ class AuthController extends Controller
     {
         try {
             $user = User::where('id', auth()->user()->id)->first();
-    
+
             $user->update([
                 'name' => $request->name,
                 'lastname' => $request->lastname,
             ]);
-    
+
             return response()->json([
                 'res' => true,
                 'msg' => 'Actualizado Tu Perfil'
@@ -93,7 +100,7 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
-        
+
         return response()->json([
             'res' => true,
             'msg' => 'Te has Deslogeado'
@@ -115,11 +122,10 @@ class AuthController extends Controller
         }
     }
 
-    public function unauthorized(Request $request){
+    public function unauthorized(Request $request)
+    {
         return response()->json([
-            "Mensaje"=>"No autorizado, No se proporcionó Token o es invalido"
-        ],401);
+            "Mensaje" => "No autorizado, No se proporcionó Token o es invalido"
+        ], 401);
     }
-
-    
 }
