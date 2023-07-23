4d84731a1d85a98444cc363f1c86f0b84b787037
sebastian-huamani <huamanitassara@gmail.com>
2023-07-23 13:54:20 -0500

    dash template data

--- app/Http/Controllers/DashboarController.php
+++ app/Http/Controllers/DashboarController.php
@@ -9,21 +9,74 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboarController extends Controller
 {
-    public function general($date){
-        $date = explode("-" ,$date) ;
+    public function general($date)
+    {
+        $date = explode("-", $date);
         $user = User::find(Auth::user()->id);
         $cards = Card::where('user_id', $user->id)->where('state_id', 1)->pluck("id");
 
-        $listItems = [];
+        $listItems = array();
         foreach ($cards as $card) {
-            $item_temp = $card->items()
+            $card_temp = Card::find($card);
+            $items_temp = $card_temp->items()
                 ->whereYear('items.created_at', $date[0])
                 ->whereMonth('items.created_at', $date[1])
-                ->orderby('items.created_at', 'desc')
-                ->pluck("created_at", "amount");
-            
-            array_push($listItems, $item_temp);
+                ->orderby('items.created_at', 'asc')
+                ->selectRaw("date_format(items.created_at, '%Y-%m-%d') as fecha, items.amount as mount")->get();
+
+            $data = $items_temp->mapToGroups(function ($item, $key) {
+                return [$item['fecha'] => $item['mount']];
+            })->toArray();
+
+            $processItems = [];
+            $sumTotal = 0;
+            foreach ($data as $key => $value) {
+                $sumTotal += round(array_sum($value), 2);
+                $item = array('x' => $key, 'y' => round($sumTotal, 2));
+                array_push($processItems, $item);
+            }
+            array_push($listItems, array('name' => $card_temp->name, 'data' => $processItems));
+            // array_push($listItems,$data);
         }
-        return $listItems;
+
+        return response()->json([
+            'res' => true,
+            'msg' => $listItems
+        ]);
+    }
+
+    public function dataxMonthTemplate($date){
+        $date = explode("-", $date);
+        $user = User::find(Auth::user()->id);
+        $cards = Card::where('user_id', $user->id)->where('state_id', 1)->pluck("id");
+
+        $listItems = array();
+        foreach ($cards as $card) {
+            $card_temp = Card::find($card);
+            $items_temp = $card_temp->items()
+                ->whereYear('items.created_at', $date[0])
+                ->whereMonth('items.created_at', $date[1])
+                ->orderby('items.created_at', 'asc')
+                ->selectRaw("date_format(items.created_at, '%Y-%m-%d') as fecha, items.amount as mount, template_id as template")->get();
+
+            $data = $items_temp->mapToGroups(function ($item, $key) {
+                return [$item['template'] => $item['mount']];
+            })->toArray();
+
+            $processItems = [];
+            $sumTotal = 0;
+            foreach ($data as $key => $value) {
+                $sumTotal += round(array_sum($value), 2);
+                $item = array('x' => $key, 'y' => round($sumTotal, 2));
+                array_push($processItems, $item);
+            }
+            array_push($listItems, array('name' => $card_temp->name, 'data' => $processItems));
+            // array_push($listItems,$data);
+        }
+
+        return response()->json([
+            'res' => true,
+            'msg' => $listItems
+        ]);
     }
 }
